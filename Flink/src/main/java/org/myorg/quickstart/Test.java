6e626856e5386d5e02b21149451962458f8038a8
Rik Schreurs <r.schreurs@ilysian.com>
2016-03-10 18:16:04 +0100

    Tweede property works.

--- Flink/src/main/java/org/myorg/quickstart/Test.java
+++ Flink/src/main/java/org/myorg/quickstart/Test.java
@@ -5,14 +5,22 @@
  */
 package org.myorg.quickstart;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import org.apache.flink.api.common.ProgramDescription;
+import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.FilterOperator;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.graph.Edge;
-import org.apache.flink.api.java.typeutils.TypeExtractor;
+import org.apache.flink.graph.EdgeDirection;
+import org.apache.flink.graph.EdgesFunctionWithVertexValue;
 import org.apache.flink.graph.Graph;
+import org.apache.flink.graph.NeighborsFunctionWithVertexValue;
 import org.apache.flink.graph.Vertex;
 import org.apache.flink.graph.gsa.ApplyFunction;
 import org.apache.flink.graph.gsa.GatherFunction;
@@ -21,71 +29,98 @@ import org.apache.flink.graph.gsa.SumFunction;
 import org.apache.flink.graph.spargel.MessageIterator;
 import org.apache.flink.graph.spargel.MessagingFunction;
 import org.apache.flink.graph.spargel.VertexUpdateFunction;
+import org.apache.flink.util.Collector;
 
 /**
  *
  * @author rikschreurs
  */
 public class Test implements ProgramDescription {
-    private static int maxIterations = 5;
-    
-    public static void main(String[] args) throws Exception {
 
+    private static int maxIterations = 50;
+
+    public static void main(String[] args) throws Exception {
+        Double total = 0.0;
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
         String filePath = "facebook";
-
+        Long srcId = new Long("56143");
         DataSet<Tuple3<Long, Long, Long>> edges = env.readCsvFile(filePath)
                 .fieldDelimiter(" ")
                 .includeFields("1101")
                 .types(Long.class, Long.class, Long.class);
+         Graph<Long, Double, Long> g = Graph.fromTupleDataSet(edges, new InitVertices(srcId), env).getUndirected();
+        List<Vertex<Long, Double>> vertices = g.getVertices().collect();
+        for (int i = 0; i < 10; i++) {
+            Graph<Long, Double, Long> graph = Graph.fromTupleDataSet(edges, new InitVertices(vertices.get(i).f0), env).getUndirected();
+            
 
-        Graph<Long, Long, Long> graph = Graph.fromTupleDataSet(edges, new MapFunction<Long, Long>() {
-            @Override
-            public Long map(Long value) throws Exception {
-                return new Long(0);
-            }
-        }, env).getUndirected();
-        
-        Graph<Long, Long, Long> result = graph.runGatherSumApplyIteration(new CalculateDistances(), new ChooseMinDistance(), new UpdateDistance(), maxIterations);
-        
-        DataSet<Vertex<Long, Long>> singleSourceShortestPaths = result.getVertices();
-        singleSourceShortestPaths.print();
+            Graph<Long, Double, Long> result = graph.runGatherSumApplyIteration(new CalculateDistances(), new ChooseMinDistance(), new UpdateDistance(), maxIterations);
+            //Graph<Long, Double, Long> result = graph.runVertexCentricIteration(new VertexDistanceUpdater(), new MinDistanceMessenger(), maxIterations);
+
+            DataSet<Vertex<Long, Double>> singleSourceShortestPaths = result.getVertices();
+            FilterOperator f = singleSourceShortestPaths.filter(new FilterFunction<Vertex<Long, Double>>() {
+                @Override
+                public boolean filter(Vertex<Long, Double> value) throws Exception {
+                    return value.getValue() != Double.POSITIVE_INFINITY;
+                }
+            });
+            List<Vertex<Long, Double>> list = f.sum(1).collect();
+            total += list.get(0).f1;
+        }
+        System.out.println("Total:" + total);
+        //singleSourceShortestPaths.print();
 
     }
-
 
     @Override
     public String getDescription() {
         return "Get All pair shortest paths Example";
     }
-    
+
     @SuppressWarnings("serial")
-	private static final class CalculateDistances extends GatherFunction<Long, Long, Long> {
+    private static final class CalculateDistances extends GatherFunction<Double, Long, Double> {
 
-                @Override
-		public Long gather(Neighbor<Long, Long> neighbor) {
-			return neighbor.getNeighborValue() + 1;
-		}
-	};
+        @Override
+        public Double gather(Neighbor<Double, Long> neighbor) {
+            return neighbor.getNeighborValue() + 1;
+        }
+    };
 
-	@SuppressWarnings("serial")
-	private static final class ChooseMinDistance extends SumFunction<Long, Long, Long> {
+    @SuppressWarnings("serial")
+    private static final class ChooseMinDistance extends SumFunction<Double, Long, Double> {
 
-                @Override
-		public Long sum(Long newValue, Long currentValue) {
-			return Math.min(newValue, currentValue);
-		}
-	};
+        public Double sum(Double newValue, Double currentValue) {
+            return Math.min(newValue, currentValue);
+        }
+    };
 
-	@SuppressWarnings("serial")
-	private static final class UpdateDistance extends ApplyFunction<Long, Long, Long> {
+    @SuppressWarnings("serial")
+    private static final class UpdateDistance extends ApplyFunction<Long, Double, Double> {
 
-                @Override
-		public void apply(Long newDistance, Long oldDistance) {
-			if (newDistance < oldDistance) {
-				setResult(newDistance);
-			}
-		}
-	}
+        @Override
+        public void apply(Double newDistance, Double oldDistance) {
+            if (newDistance < oldDistance) {
+                setResult(newDistance);
+            }
+        }
+    };
+
+    public static final class InitVertices implements MapFunction<Long, Double> {
+
+        private final long srcId;
+
+        public InitVertices(long srcId) {
+            this.srcId = srcId;
+        }
+
+        @Override
+        public Double map(Long id) {
+            if (id.equals(srcId)) {
+                return 0.0;
+            } else {
+                return Double.POSITIVE_INFINITY;
+            }
+        }
+    }
 }
